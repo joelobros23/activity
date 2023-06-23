f65c73184ff6769b2b646d4177d785bf55d666bf
joelobros23 <laberintoj8@gmail.com>
2023-06-23 13:52:35 +0800

    firstname

--- frontend/index.php
+++ frontend/index.php
@@ -14,7 +14,13 @@
       </div>
       <div class="card-body">
         <form action="register_process.php" method="POST">
+        <div class="form-group row">
+            <label for="firstname" class="col-sm-2 col-form-label">First Name</label>
+            <div class="col-sm-4">
+              <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Enter your first name" required>
+            </div>
 
+          <button type="submit" class="btn btn-primary">Register</button>
         </form>
       </div>
     </div>
