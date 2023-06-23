6719678c174208461a7f70f8a3d937edd41b3732
joelobros23 <laberintoj8@gmail.com>
2023-06-23 14:45:53 +0800

    adding bootstraps

--- frontend/login.php
+++ frontend/login.php
@@ -4,8 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
-    
+    <div class="container mt-5">
+        <div class="card col-sm-6 mx-auto">
+            <div class="card-header">
+                Login
+            </div>
+            <div class="card-body">
+                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" id="email" name="email" value="<?php echo $email; ?>">
+                        <div class="invalid-feedback"><?php echo $email_err; ?></div>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input type="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" id="password" name="password">
+                        <div class="invalid-feedback"><?php echo $password_err; ?></div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
