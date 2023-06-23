972e08ca0d1fb885c10391ae868aae1da115bed7
joelobros23 <laberintoj8@gmail.com>
2023-06-23 13:52:06 +0800

    form

--- frontend/index.php
+++ frontend/index.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Activity</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
-    
+<div class="container mt-5">
+    <div class="card">
+      <div class="card-header">
+        Register
+      </div>
+      <div class="card-body">
+        <form action="register_process.php" method="POST">
+
+        </form>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
