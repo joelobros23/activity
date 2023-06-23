29f7e5a97836d87f2859d6a5086142f77aa42966
joelobros23 <laberintoj8@gmail.com>
2023-06-23 14:24:11 +0800

    qwe

--- frontend/index.php
+++ frontend/index.php
@@ -7,34 +7,32 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
-<div class="container mt-5">
-    <div class="card">
+<div class="container mt-5 d-flex justify-content-center">
+    <div class="card col-sm-7">
       <div class="card-header">
         Register
       </div>
       <div class="card-body">
-        <form action="register_process.php" method="POST">
+        <form action="register_process.php" method="POST col-sm-9 d-flex justify-content-center">
         <div class="form-group row">
-            <div class="col-sm-4">
+            <div class="col-sm-6">
               <input type="text" class="form-control" id="firstname" name="firstname" placeholder="First Name" required>
             </div>
-            <div class="col-sm-4">
+            <div class="col-sm-6">
               <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Last Name" required>
             </div>
           </div>
-          <div class="form-group">
-
+          <div class="form-group col-sm-12">
             <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
           </div>
-          <div class="form-group">
-
+          <div class="form-group col-sm-12">
             <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
           </div>
-          <div class="form-group">
+          <div class="form-group col-sm-12 mb-5">
             <label for="birthdate">Birthdate</label>
             <input type="date" class="form-control" id="birthdate" name="birthdate" required>
           </div>
-          <button type="submit" class="btn btn-primary">Register</button>
+          <button type="submit" class="btn btn-primary d-flex justify-content-center">Register</button>
         </form>
       </div>
     </div>
