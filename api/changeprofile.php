e550188ed24f228d308f8ebc13bdcbdd8c88dfaf
joelobros23 <laberintoj8@gmail.com>
2023-06-23 17:44:32 +0800

    adding comments

--- api/changeprofile.php
+++ api/changeprofile.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         echo "Profile information updated.";
     } else {
-        // Error occurred while updating profile information
+
         echo "Error: " . $stmt->error;
     }
 
