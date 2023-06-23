79fa96b75d54aa0555cbabc7355f6bef454fe215
joelobros23 <laberintoj8@gmail.com>
2023-06-23 16:14:00 +0800

    adding query and pooper

--- frontend/index.php
+++ frontend/index.php
@@ -75,5 +75,9 @@ $stmt->close();
       </div>
     </div>
   </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
