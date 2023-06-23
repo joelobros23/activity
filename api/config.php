631fda62076032ce5bd7e01051a1097225568edf
joelobros23 <laberintoj8@gmail.com>
2023-06-23 16:06:15 +0800

    adding something in config.php

--- api/config.php
+++ api/config.php
@@ -1,9 +1,9 @@
 <?php
-// Database configuration constants
-define('DB_HOST', 'localhost');  // Replace with your database host
-define('DB_USERNAME', 'root');   // Replace with your database username
-define('DB_PASSWORD', '');       // Replace with your database password
-define('DB_NAME', 'kodego_db');  // Replace with your database name
+
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');  
+define('DB_NAME', 'kodego_db');
 
 // Create a database connection
 $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
