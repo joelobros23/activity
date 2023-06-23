9b1fb2863e130803589c0ba32ec79d67d21c301c
joelobros23 <laberintoj8@gmail.com>
2023-06-23 16:06:36 +0800

    removing something in config.php

--- api/config.php
+++ api/config.php
@@ -5,10 +5,10 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');  
 define('DB_NAME', 'kodego_db');
 
-// Create a database connection
+
 $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
