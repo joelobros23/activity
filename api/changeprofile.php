db54b68434e2fbae5e313320b075a423fc52b743
joelobros23 <laberintoj8@gmail.com>
2023-06-23 17:43:49 +0800

    adding some elements

--- api/changeprofile.php
+++ api/changeprofile.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $user_id = $_SESSION['user_id'];
 
-    // Update user data in the database
+
     $sql = "UPDATE users SET firstname = ?, lastname = ?, email = ? WHERE id = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("sssi", $name, $lastname, $email, $user_id);
